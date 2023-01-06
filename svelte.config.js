40443b78bbad75bbf544687980ae67afd974b8d4
Darin Evanow <darin.evanow@gusto.com>
2023-01-05 17:46:52 -0800

    Update Sveltekit to v1 and Threlte to v5.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import sequence from 'svelte-sequential-preprocessor';
 import preprocess from 'svelte-preprocess';
+import { preprocessThrelte } from '@threlte/preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sequence([preprocess(), preprocessThrelte()]),
 
 	kit: {
 		adapter: adapter()
